a2009358a8f64807fa9eb5ffb15322b5175b43cc
Honeshwar <73346917+Honeshwar@users.noreply.github.com>
2024-01-08 10:53:51 +0530

    Update makeRequestAPI.js

--- src/utils/makeRequestAPI.js
+++ src/utils/makeRequestAPI.js
@@ -4,9 +4,9 @@ const baseUrl = process.env.REACT_APP_API_URL;
 
 const makeRequestAPI = axios.create({
   baseURL: baseUrl,
-  headers: {
-    Authorization: "bearer " + process.env.REACT_APP_API_TOKEN, //Some synonyms for word bearer in English are holder, or carrier.
-  },
+  // headers: {
+  //   Authorization: "bearer " + process.env.REACT_APP_API_TOKEN, //Some synonyms for word bearer in English are holder, or carrier.
+  // },
 });
 
 export default makeRequestAPI;
