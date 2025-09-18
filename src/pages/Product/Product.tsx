d37af05052e64d7816043903a64f951d4095dc26
Honeshwar Thakur <honeshwar.thakur@tailnode.com>
2025-09-18 23:02:42 +0530

    adding product level validation

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -63,90 +63,92 @@ function Products() {
       ) : loading ? (
         <Loader />
       ) : (
-        <>
-          <div className="left">
-            <div className="images">
-              <img
-                src={
-                  (data as ApiResponse<Product>)?.attributes?.img?.data
-                    ?.attributes?.formats?.thumbnail?.url
-                }
-                alt={"product"}
-                onClick={() => setSelectedImage("img")}
-              />
-              <img
-                src={
-                  (data as ApiResponse<Product>)?.attributes?.img2?.data
-                    ?.attributes?.formats?.thumbnail?.url
-                }
-                alt={"product"}
-                onClick={() => setSelectedImage("img2")}
-              />
-            </div>
-            <div className="mainImg">
-              <img
-                src={
-                  data?.attributes[selectedImage as "img" | "img2"]?.data
-                    ?.attributes?.formats?.small?.url
-                }
-                alt={"product"}
-              />
-            </div>
-          </div>
-          <div className="right">
-            <h1>{data?.attributes?.title}</h1>
-            <span className="price">Rs {data?.attributes?.price}</span>
-            <p>{data?.attributes?.desc}</p>
-            <div className="quantity">
-              <button
-                type="button"
-                onClick={() =>
-                  setQuantity((prev) => (prev === 1 ? 1 : prev - 1))
-                }
-              >
-                -
-              </button>
-              {quantity}
-              <button
-                type="button"
-                onClick={() => setQuantity((prev) => prev + 1)}
-              >
-                +
-              </button>
-            </div>
-            <button
-              type="button"
-              className="addToCart"
-              onClick={addToCartHandler}
-            >
-              <BiSolidCartDownload />
-              Add to cart
-            </button>
-            <div className="link">
-              <div className="linkItems">
-                <AiOutlineHeart />
-                Add to wishlist
+        data && (
+          <>
+            <div className="left">
+              <div className="images">
+                <img
+                  src={
+                    data?.attributes?.img?.data?.attributes?.formats?.thumbnail
+                      ?.url
+                  }
+                  alt={"product"}
+                  onClick={() => setSelectedImage("img")}
+                />
+                <img
+                  src={
+                    data?.attributes?.img2?.data?.attributes?.formats?.thumbnail
+                      ?.url
+                  }
+                  alt={"product"}
+                  onClick={() => setSelectedImage("img2")}
+                />
               </div>
-              <div className="linkItems">
-                <MdBalance />
-                Add to compare
+              <div className="mainImg">
+                <img
+                  src={
+                    data?.attributes[selectedImage as "img" | "img2"]?.data
+                      ?.attributes?.formats?.small?.url
+                  }
+                  alt={"product"}
+                />
               </div>
             </div>
-            <div className="info">
-              <span>Vendor Polo</span>
-              <span>Product Type: T-Shirt</span>
-              <span>Tags: T-Shirt, Men, Top</span>
-            </div>
-            <hr />
-            <div className="info">
-              <span>DESCRIPTION</span>
+            <div className="right">
+              <h1>{data?.attributes?.title}</h1>
+              <span className="price">Rs {data?.attributes?.price}</span>
+              <p>{data?.attributes?.desc}</p>
+              <div className="quantity">
+                <button
+                  type="button"
+                  onClick={() =>
+                    setQuantity((prev) => (prev === 1 ? 1 : prev - 1))
+                  }
+                >
+                  -
+                </button>
+                {quantity}
+                <button
+                  type="button"
+                  onClick={() => setQuantity((prev) => prev + 1)}
+                >
+                  +
+                </button>
+              </div>
+              <button
+                type="button"
+                className="addToCart"
+                onClick={addToCartHandler}
+              >
+                <BiSolidCartDownload />
+                Add to cart
+              </button>
+              <div className="link">
+                <div className="linkItems">
+                  <AiOutlineHeart />
+                  Add to wishlist
+                </div>
+                <div className="linkItems">
+                  <MdBalance />
+                  Add to compare
+                </div>
+              </div>
+              <div className="info">
+                <span>Vendor Polo</span>
+                <span>Product Type: T-Shirt</span>
+                <span>Tags: T-Shirt, Men, Top</span>
+              </div>
               <hr />
-              <span>ADDITIONAL INFORMATION</span>
-              <hr />
-              <span>FAQ</span>
+              <div className="info">
+                <span>DESCRIPTION</span>
+                <hr />
+                <span>ADDITIONAL INFORMATION</span>
+                <hr />
+                <span>FAQ</span>
+              </div>
             </div>
-          </div>
-        </>
+          </>
+        )
       )}
     </div>
   );
